4904e08b08aa0c8b563aac5a3f6e464a10aae8e4
Arnob <arnoblalam@gmail.com>
2014-12-22 08:28:19 -0800

    Trying to add wreq

--- src/Client.hs
+++ src/Client.hs
@@ -1,12 +1,18 @@
 module Client where
 
+import GHC.Generics
+
 import Settings
 import Network.HTTP.Client
 import qualified Data.ByteString as B
-import qualified Data.ByteString as LBS
 import Network.HTTP.Client.TLS
-import Data.Aeson
+import Data.Aeson.Generic
 
+data Queue = Queue {
+    name :: String,
+    projectID :: String,
+    id :: String
+} deriving (Generic)
 
 doRequest method server projectID endpoint token body = do manager <- newManager tlsManagerSettings
                                                            request <- makeRequest url token method body
@@ -16,7 +22,7 @@ doRequest method server projectID endpoint token body = do manager <- newManager
                                                                 makeRequest url token method body = do initReq <- parseUrl url
                                                                                                        let request = initReq {
                                                                                                            requestHeaders = [
-                                                                                                           ("Authorization", (B.append "OAuth " token)), 
+                                                                                                           ("Authorization", B.append "OAuth " token), 
                                                                                                            ("Content-Type", "application/json")],
                                                                                                            method = method
                                                                                                        }
@@ -25,10 +31,6 @@ doRequest method server projectID endpoint token body = do manager <- newManager
 request method endpoint body = doRequest method defaultServer defaultProjectID endpoint defaultToken body
 
 queues = do response <- request "GET" "queues" ""
-            let json = (decode $ responseBody response)::Maybe Array
-            case json of
-                Just something -> print something
-                Nothing -> return ()
-
+            print $ responseBody response
 
 main = queues
