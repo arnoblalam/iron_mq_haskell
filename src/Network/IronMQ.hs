f46324757c6db2eba6150e3e84d5b571634f64ab
Arnob Alam <arnoblalam@gmail.com>
2014-12-23 14:46:47 -0500

    Add documentation

--- src/Network/IronMQ.hs
+++ src/Network/IronMQ.hs
@@ -7,23 +7,31 @@ import Data.Aeson (FromJSON, ToJSON)
 import Data.Text (Text)
 import Network.IronMQ.Types
 
+
+-- | Make a get request to an endpoint using connection info from client and
+-- query string set to parameters. Return the JSON results
 getJSONWithOpts :: FromJSON a => Client -> String -> [(Text, Text)] -> IO a
-getJSONWithOpts client s ps = do
+getJSONWithOpts client endpoint parameters = do
     let baseurl = concat ["https://", server client, "/", apiVersion client, "/projects/", projectID client]
-        url = baseurl ++ s
-        getOpts = defaults & header "Content-Type" .~ ["application/json"] & params .~ ("oauth", token client) : ps
+        url = baseurl ++ endpoint
+        getOpts = defaults & header "Content-Type" .~ ["application/json"] & params .~ ("oauth", token client) : parameters
     response <- asJSON =<< getWith getOpts url
     return (response ^. responseBody)
 
+-- | Make a get request to an endpoint using the connection info from client.
+-- Return the JSON results.
 getJSON ::FromJSON a => Client -> String -> IO a
 getJSON client s = getJSONWithOpts client s []
 
+-- | Get a list of queues available to the client
 getQueues :: Client -> IO [QueueSummary]
 getQueues client = getJSON client "/queues"
 
+-- | Get information about a queue from a client
 getQueue :: Client -> String -> IO Queue
 getQueue client queueName = getJSON client ("/queues/" ++ queueName)
 
+-- | Get a list of messages from the Client
 getMessages :: Client -> String -> IO MessageList
 getMessages client queueName = getJSON client ("/queues/" ++ queueName ++ "/messages")
 
