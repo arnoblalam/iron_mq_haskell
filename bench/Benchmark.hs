9c9b6349e691e47d17296e81d46ac4b36bf254ad
Arnob Alam <arnoblalam@gmail.com>
2014-12-27 15:10:45 -0500

    Add type annotations to Benchmark.hs, explicitly discard result of queues call

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Criterion.Main
-import Data.Text (append, pack)
 
 import Network.IronMQ
 import Network.IronMQ.Types
 
+main :: IO ()
 main = defaultMain [bench "get queue info, post a message, get messages, delete message" $ nfIO (doStuff)]
 
+testClient :: Client
 testClient = Client {
     token = "_B6KOfA16D4AmW2NwSCw12mgVxk",
     projectID = "53f691bd45d4960005000082",
@@ -15,8 +16,10 @@ testClient = Client {
     apiVersion = "1"
 }
 
+
+doStuff :: IO ()
 doStuff = do
-    qs <- queues testClient
+    _ <- queues testClient
     postMessages testClient "default" [message{mBody = "This is message number "}]
     messageList <- getMessages testClient "default"
     let messageID = mId (head (messages messageList))
